0db7a38c7e8c36d2144a7a3888220d292a82304e
Filip Kuszenin <filipkuszenin@gmail.com>
2018-10-11 15:05:10 +0100

    Game.checkWin created and tested

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,2 +1,22 @@
 public class Game {
+    private Deck deck;
+    private Player player1;
+    private Player player2;
+
+    public Game(Deck deck, Player player1, Player player2) {
+        this.deck = deck;
+        this.player1 = player1;
+        this.player1 = player2;
+    }
+
+    public Card checkWin(Card card1, Card card2) {
+        Card winningCard;
+
+        if (card1.getValueFromEnum() > card2.getValueFromEnum()) {
+            winningCard = card1;
+        } else {
+            winningCard = card2;
+        }
+        return winningCard;
+    }
 }
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,2 +1,29 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class GameTest {
+    Game game;
+    Deck deck;
+    Player player1;
+    Player player2;
+    Card card1;
+    Card card2;
+
+    @Before
+    public void before() {
+        deck = new Deck();
+        player1 = new Player();
+        player2 = new Player();
+        card1 = new Card(SuitType.CLUBS, RankType.ACE);
+        card2 = new Card(SuitType.HEARTS, RankType.THREE);
+        game = new Game(deck, player1, player2);
+    }
+
+    @Test
+    public void hasCheckWin() {
+
+        assertEquals(card1, game.checkWin(card1, card2));
+    }
 }
