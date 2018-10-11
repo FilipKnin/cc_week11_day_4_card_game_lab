f006edaab8603771cf7eb30ae6d5f8939127bc49
Filip Kuszenin <filipkuszenin@gmail.com>
2018-10-11 13:29:26 +0100

    Card.checkDeck created and tested

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -1,2 +1,13 @@
+import java.util.ArrayList;
+
 public class Deck {
+    private ArrayList<Card> deck;
+
+    public Deck() {
+        this.deck = new ArrayList<>();
+    }
+
+    public int checkDeck() {
+        return this.deck.size();
+    }
 }
--- src/test/java/DeckTest.java
+++ src/test/java/DeckTest.java
@@ -1,2 +1,24 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
 public class DeckTest {
+    Deck deck;
+    Card card1;
+    Card card2;
+
+    @Before
+    public void before() {
+        deck = new Deck();
+        card1 = new Card(SuitType.CLUBS, RankType.ACE);
+        card2 = new Card(SuitType.HEARTS, RankType.THREE);
+    }
+
+    @Test
+    public void hasEmptyDeck() {
+        assertEquals(0, deck.checkDeck());
+    }
 }
