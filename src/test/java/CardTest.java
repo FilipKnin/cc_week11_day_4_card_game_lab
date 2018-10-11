c165d78b1ff05bc03c43caadc5c7d742e0f43dd0
Filip Kuszenin <filipkuszenin@gmail.com>
2018-10-11 13:13:11 +0100

    Card class created and tested/SuitType and RankType created and tested

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -1,2 +1,23 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
 public class CardTest {
+
+    Card card;
+
+    @Before
+    public void before() {
+        card = new Card(SuitType.HEARTS, RankType.ACE);
+    }
+
+    @Test
+    public void canGetSuit() {
+        assertEquals(SuitType.HEARTS, card.getSuit());
+    }
+
+    @Test
+    public void canRankType() {
+        assertEquals(RankType.ACE, card.getRank());
+    }
+
 }
